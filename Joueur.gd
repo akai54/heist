8948f893f6268742fddb9c584b7d702f26fce0d9
ALADDAM Besher <beshoux@outlook.fr>
2021-04-28 07:58:18 +0200

    Utilisation du [lerp].

--- Joueur.gd
+++ Joueur.gd
@@ -10,17 +10,21 @@ func _physics_process(delta):
 	move_and_slide(mouvement)
 
 func maj_mouvement():
+
+	
 	# clamp Fixe la valeur et renvoie une valeur non inférieure à min et pas supérieure à max.
 	if Input.is_action_pressed("se_déplacer_vers_le_bas") and not Input.is_action_pressed("se_déplacer_vers_le_haut") :
 		mouvement.y = clamp(mouvement.y + VITESSE, 0, VITESSE_MAX)
 	elif Input.is_action_pressed("se_déplacer_vers_le_haut") and not Input.is_action_pressed("se_déplacer_vers_le_bas") :
 		mouvement.y = clamp(mouvement.y - VITESSE, -VITESSE_MAX, 0)
+
+	# Interpole linéairement entre deux valeurs par une valeur normalisée.
 	else:
-		mouvement.y = 0
+		mouvement.y = lerp(mouvement.y, 0, FRICTION)
 
 	if Input.is_action_pressed("se_déplacer_vers_la_gauche") and not Input.is_action_pressed("se_déplacer_vers_la_droite") :
 		mouvement.x = clamp(mouvement.x - VITESSE, -VITESSE_MAX, 0)
 	elif Input.is_action_pressed("se_déplacer_vers_la_droite") and not Input.is_action_pressed("se_déplacer_vers_la_gauche") :
 		mouvement.x = clamp(mouvement.x + VITESSE, 0, VITESSE_MAX)
 	else:
-		mouvement.x = 0
+		mouvement.x = lerp(mouvement.x, 0, FRICTION)
