9a36d9769a6a553ed9b68acc731e0c7955fab60e
ALADDAM Besher <beshoux@outlook.fr>
2021-04-28 07:42:13 +0200

    Utilisation du [clamp].

--- Joueur.gd
+++ Joueur.gd
@@ -6,20 +6,21 @@ var mouvement = Vector2()
 # car on utilise un KinematicBody2D.
 
 func _physics_process(delta):
-	update_mouvement()
+	maj_mouvement()
 	move_and_slide(mouvement)
 
-func update_mouvement():
+func maj_mouvement():
+	# clamp Fixe la valeur et renvoie une valeur non inférieure à min et pas supérieure à max.
 	if Input.is_action_pressed("se_déplacer_vers_le_bas") and not Input.is_action_pressed("se_déplacer_vers_le_haut") :
-		mouvement.y += VITESSE
+		mouvement.y = clamp(mouvement.y + VITESSE, 0, VITESSE_MAX)
 	elif Input.is_action_pressed("se_déplacer_vers_le_haut") and not Input.is_action_pressed("se_déplacer_vers_le_bas") :
-		mouvement.y -= VITESSE
+		mouvement.y = clamp(mouvement.y - VITESSE, -VITESSE_MAX, 0)
 	else:
 		mouvement.y = 0
 
 	if Input.is_action_pressed("se_déplacer_vers_la_gauche") and not Input.is_action_pressed("se_déplacer_vers_la_droite") :
-		mouvement.x -= VITESSE
+		mouvement.x = clamp(mouvement.x - VITESSE, -VITESSE_MAX, 0)
 	elif Input.is_action_pressed("se_déplacer_vers_la_droite") and not Input.is_action_pressed("se_déplacer_vers_la_gauche") :
-		mouvement.x += VITESSE
+		mouvement.x = clamp(mouvement.x + VITESSE, 0, VITESSE_MAX)
 	else:
 		mouvement.x = 0
